b224dfa09b3442272daf6f7dc1f4e7746fc9151f
Waldir Maidana Valenzuela <waldirmaidana@gmail.com>
2025-02-12 22:07:07 -0500

    ci: Mejoras apra el despliegue en vercel

---
+++ environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true, // Change this to true for production
+    API_URL: 'https://nanotechstore.store/backend/public/api/', // Change this to your API URL
+};
